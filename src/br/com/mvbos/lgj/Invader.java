7206ae2431458386ae52929009f9ff0a0a576c01
Takanori42 <gaby.nagato123@gmail.com>
2023-09-28 18:05:10 -0300

    mudanças

--- src/br/com/mvbos/lgj/Invader.java
+++ src/br/com/mvbos/lgj/Invader.java
@@ -1,9 +1,16 @@
-package spaceinvaders;
+package br.com.mvbos.lgj;
 
+import br.com.mvbos.lgj.Elemento;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class Invader extends Elemento {
+    public static void main(String[] args) {
+
+    }
+
+    private int contador, contadorEspera, moverInimigos, totalInimigos, destruidos, level;
+    boolean colideBordas = false;
 
     private Invader[][] invasores = new Invader[11][5];
     private Invader.Tipos[] tipoPorLinha = { Tipos.PEQUENO,
@@ -23,5 +30,42 @@ public class Invader extends Elemento {
             invasores[i][j] = e;
         }
     }
+
+    if(contador>contadorEspera)
+
+    {
+        moverInimigos = true;
+        contador = 0;
+        contadorEspera = totalInimigos - destruidos - level * level;
+    }else
+    {
+        contador++;
+    }if(tanque.isAtivo())
+    {
+        if (controleTecla[2]) {
+            tanque.setPx(tanque.getPx() - tanque.getVel());
+        } else if (controleTecla[3]) {
+            tanque.setPx(tanque.getPx() + tanque.getVel());
+        }
+    }
+    // pressionou espaço, adiciona tiro
+    if(controleTecla[4]&&!tiroTanque.isAtivo())
+    {
+        tiroTanque.setPx(tanque.getPx() + tanque.getLargura() / 2
+                - tiroTanque.getLargura() / 2);
+        tiroTanque.setPy(tanque.getPy() - tiroTanque.getAltura());
+        tiroTanque.setAtivo(true);
+    }if(chefe.isAtivo())
+    {
+        chefe.incPx(tanque.getVel() - 1);
+
+        if (!tiroChefe.isAtivo() && Util.colideX(chefe, tanque)) {
+            addTiroInimigo(chefe, tiroChefe);
+        }
+        if (chefe.getPx() > tela.getWidth()) {
+            chefe.setAtivo(false);
+        }
+    }
+}
 
 }
